GITNE/JOSM

commit 98bc6dc946e5532cd5edb6bcea1c303ca94bf1ac

fix #9984 - Add support for WMS tiles defining a transparent color in RGB space (tRNS PNG chunk for example), instead of a proper alpha channel. Surprisingly, Java does not support that out of the box, `ImageIO.read` always returns opaque images. Allows to switch between this mode and standard mode using WMS layer contextual entry "Use Alpha Channel", for consistency with images defining an alpha channel. Does not impact other images than WMS tiles.
git-svn-id: https://josm.openstreetmap.de/svn/trunk@7132 0c6e7542-c601-0410-84e7-c038aed88b3b

test/unit/org/openstreetmap/josm/tools/ImageProviderTest.java
@@ -1,0 +1,31 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.tools;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.awt.Transparency;
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Test;
+import org.openstreetmap.josm.TestUtils;
+
+/**
+ * Unit tests of {@link ImageProvider} class.
+ */
+public class ImageProviderTest {
+
+    /**
+     * Non-regression test for ticket <a href="https://josm.openstreetmap.de/ticket/9984">#9984</a>
+     * @throws IOException if an error occurs during reading
+     */
+    @Test
+    public void testTicket9984() throws IOException {
+        File file = new File(TestUtils.getTestDataRoot()+"regress/9984/tile.png");
+        assertThat(ImageProvider.read(file, true, true).getTransparency(), is(Transparency.TRANSLUCENT));
+        assertThat(ImageProvider.read(file, false, true).getTransparency(), is(Transparency.TRANSLUCENT));
+        assertThat(ImageProvider.read(file, false, false).getTransparency(), is(Transparency.OPAQUE));
+        assertThat(ImageProvider.read(file, true, false).getTransparency(), is(Transparency.OPAQUE));
+    }
+}

src/org/openstreetmap/josm/data/imagery/GeorefImage.java
@@ -21,6 +21,7 @@ import org.openstreetmap.josm.data.coor.EastNorth;
 import org.openstreetmap.josm.gui.NavigatableComponent;
 import org.openstreetmap.josm.gui.layer.ImageryLayer;
 import org.openstreetmap.josm.gui.layer.WMSLayer;
+import org.openstreetmap.josm.tools.ImageProvider;
 
 public class GeorefImage implements Serializable {
     private static final long serialVersionUID = 1L;
@@ -46,7 +47,6 @@ public class GeorefImage implements Serializable {
         return layer.getEastNorth(xIndex+1, yIndex+1);
     }
 
-
     public GeorefImage(WMSLayer layer) {
         this.layer = layer;
     }
@@ -56,7 +56,7 @@ public class GeorefImage implements Serializable {
             this.xIndex = xIndex;
             this.yIndex = yIndex;
             this.image = null;
-            flushedResizedCachedInstance();
+            flushResizedCachedInstance();
         }
     }
 
@@ -64,11 +64,23 @@ public class GeorefImage implements Serializable {
         return this.xIndex == xIndex && this.yIndex == yIndex;
     }
 
+    /**
+     * Resets this image to initial state and release all resources being used.
+     * @since 7132
+     */
+    public void resetImage() {
+        if (image != null) {
+            image.flush();
+        }
+        changeImage(null, null);
+    }
+
     public void changeImage(State state, BufferedImage image) {
-        flushedResizedCachedInstance();
+        flushResizedCachedInstance();
         this.image = image;
         this.state = state;
-
+        if (state == null)
+            return;
         switch (state) {
         case FAILED:
             BufferedImage imgFailed = createImage();
@@ -102,7 +114,7 @@ public class GeorefImage implements Serializable {
     }
 
     public boolean paint(Graphics g, NavigatableComponent nc, int xIndex, int yIndex, int leftEdge, int bottomEdge) {
-        if (image == null)
+        if (getImage() == null)
             return false;
 
         if(!(this.xIndex == xIndex && this.yIndex == yIndex))
@@ -173,7 +185,7 @@ public class GeorefImage implements Serializable {
     }
 
     private void fallbackDraw(Graphics g, Image img, int x, int y, int width, int height, boolean alphaChannel) {
-        flushedResizedCachedInstance();
+        flushResizedCachedInstance();
         g.drawImage(
                 img, x, y, x + width, y + height,
                 0, 0, img.getWidth(null), img.getHeight(null),
@@ -194,7 +206,7 @@ public class GeorefImage implements Serializable {
         state = (State) in.readObject();
         boolean hasImage = in.readBoolean();
         if (hasImage) {
-            image = (ImageIO.read(ImageIO.createImageInputStream(in)));
+            image = ImageProvider.read(ImageIO.createImageInputStream(in), true, WMSLayer.PROP_ALPHA_CHANNEL.get());
         } else {
             in.readObject(); // read null from input stream
             image = null;
@@ -212,7 +224,7 @@ public class GeorefImage implements Serializable {
         }
     }
 
-    public void flushedResizedCachedInstance() {
+    public void flushResizedCachedInstance() {
         if (reImg != null) {
             BufferedImage img = reImg.get();
             if (img != null) {
@@ -221,7 +233,6 @@ public class GeorefImage implements Serializable {
         }
         reImg = null;
     }
-
 
     public BufferedImage getImage() {
         return image;

src/org/openstreetmap/josm/data/imagery/WmsCache.java
@@ -41,13 +41,15 @@ import org.openstreetmap.josm.data.imagery.types.ProjectionType;
 import org.openstreetmap.josm.data.imagery.types.WmsCacheType;
 import org.openstreetmap.josm.data.preferences.StringProperty;
 import org.openstreetmap.josm.data.projection.Projection;
+import org.openstreetmap.josm.gui.layer.WMSLayer;
+import org.openstreetmap.josm.tools.ImageProvider;
 import org.openstreetmap.josm.tools.Utils;
 
 public class WmsCache {
     //TODO Property for maximum cache size
     //TODO Property for maximum age of tile, automatically remove old tiles
     //TODO Measure time for partially loading from cache, compare with time to download tile. If slower, disable partial cache
-    //TODO Do loading from partial cache and downloading at the same time, don't wait for partical cache to load
+    //TODO Do loading from partial cache and downloading at the same time, don't wait for partial cache to load
 
     private static final StringProperty PROP_CACHE_PATH = new StringProperty("imagery.wms-cache.path", "wms");
     private static final String INDEX_FILENAME = "index.xml";
@@ -69,6 +71,13 @@ public class WmsCache {
             this.north = north;
             this.bounds = new ProjectionBounds(east, north, east + tileSize / pixelPerDegree, north + tileSize / pixelPerDegree);
             this.filename = filename;
+        }
+
+        @Override
+        public String toString() {
+            return "CacheEntry [pixelPerDegree=" + pixelPerDegree + ", east=" + east + ", north=" + north + ", bounds="
+                    + bounds + ", filename=" + filename + ", lastUsed=" + lastUsed + ", lastModified=" + lastModified
+                    + "]";
         }
     }
 
@@ -298,22 +307,26 @@ public class WmsCache {
         return new File(cacheDir, projection.cacheDirectory + "/" + entry.filename);
     }
 
-    private BufferedImage loadImage(ProjectionEntries projectionEntries, CacheEntry entry) throws IOException {
-
+    private BufferedImage loadImage(ProjectionEntries projectionEntries, CacheEntry entry, boolean enforceTransparency) throws IOException {
         synchronized (this) {
             entry.lastUsed = System.currentTimeMillis();
 
             SoftReference<BufferedImage> memCache = memoryCache.get(entry);
             if (memCache != null) {
                 BufferedImage result = memCache.get();
-                if (result != null)
-                    return result;
+                if (result != null) {
+                    if (enforceTransparency == ImageProvider.isTransparencyForced(result)) {
+                        return result;
+                    } else if (Main.isDebugEnabled()) {
+                        Main.debug("Skipping "+entry+" from memory cache (transparency enforcement)");
+                    }
+                }
             }
         }
 
         try {
             // Reading can't be in synchronized section, it's too slow
-            BufferedImage result = ImageIO.read(getImageFile(projectionEntries, entry));
+            BufferedImage result = ImageProvider.read(getImageFile(projectionEntries, entry), true, enforceTransparency);
             synchronized (this) {
                 if (result == null) {
                     projectionEntries.entries.remove(entry);
@@ -353,7 +366,7 @@ public class WmsCache {
         }
         if (entry != null) {
             try {
-                return loadImage(projectionEntries, entry);
+                return loadImage(projectionEntries, entry, WMSLayer.PROP_ALPHA_CHANNEL.get());
             } catch (IOException e) {
                 Main.error("Unable to load file from wms cache");
                 Main.error(e);
@@ -363,7 +376,7 @@ public class WmsCache {
         return null;
     }
 
-    public  BufferedImage getPartialMatch(Projection projection, double pixelPerDegree, double east, double north) {
+    public BufferedImage getPartialMatch(Projection projection, double pixelPerDegree, double east, double north) {
         ProjectionEntries projectionEntries;
         List<CacheEntry> matches;
         synchronized (this) {
@@ -389,7 +402,6 @@ public class WmsCache {
             if (matches.isEmpty())
                 return null;
 
-
             Collections.sort(matches, new Comparator<CacheEntry>() {
                 @Override
                 public int compare(CacheEntry o1, CacheEntry o2) {
@@ -398,17 +410,19 @@ public class WmsCache {
             });
         }
 
-        //TODO Use alpha layer only when enabled on wms layer
-        BufferedImage result = new BufferedImage(tileSize, tileSize, BufferedImage.TYPE_4BYTE_ABGR);
+        // Use alpha layer only when enabled on wms layer
+        boolean alpha = WMSLayer.PROP_ALPHA_CHANNEL.get();
+        BufferedImage result = new BufferedImage(tileSize, tileSize,
+                alpha ? BufferedImage.TYPE_4BYTE_ABGR : BufferedImage.TYPE_3BYTE_BGR);
         Graphics2D g = result.createGraphics();
-
 
         boolean drawAtLeastOnce = false;
         Map<CacheEntry, SoftReference<BufferedImage>> localCache = new HashMap<>();
         for (CacheEntry ce: matches) {
             BufferedImage img;
             try {
-                img = loadImage(projectionEntries, ce);
+                // Enforce transparency only when alpha enabled on wms layer too
+                img = loadImage(projectionEntries, ce, alpha);
                 localCache.put(ce, new SoftReference<>(img));
             } catch (IOException e) {
                 continue;

src/org/openstreetmap/josm/gui/layer/WMSLayer.java
@@ -65,6 +65,7 @@ import org.openstreetmap.josm.io.imagery.Grabber;
 import org.openstreetmap.josm.io.imagery.HTMLGrabber;
 import org.openstreetmap.josm.io.imagery.WMSGrabber;
 import org.openstreetmap.josm.io.imagery.WMSRequest;
+import org.openstreetmap.josm.tools.ImageProvider;
 
 /**
  * This is a layer that grabs the current screen from an WMS server. The data
@@ -812,12 +813,19 @@ public class WMSLayer extends ImageryLayer implements ImageObserver, PreferenceC
             JCheckBoxMenuItem checkbox = (JCheckBoxMenuItem) ev.getSource();
             boolean alphaChannel = checkbox.isSelected();
             PROP_ALPHA_CHANNEL.put(alphaChannel);
+            Main.info("WMS Alpha channel changed to "+alphaChannel);
 
             // clear all resized cached instances and repaint the layer
             for (int x = 0; x < dax; ++x) {
                 for (int y = 0; y < day; ++y) {
                     GeorefImage img = images[modulo(x, dax)][modulo(y, day)];
-                    img.flushedResizedCachedInstance();
+                    img.flushResizedCachedInstance();
+                    BufferedImage bi = img.getImage();
+                    // Completely erases images for which transparency has been forced,
+                    // or images that should be forced now, as they need to be recreated
+                    if (ImageProvider.isTransparencyForced(bi) || ImageProvider.hasTransparentColor(bi)) {
+                        img.resetImage();
+                    }
                 }
             }
             Main.map.mapView.repaint();

src/org/openstreetmap/josm/io/CacheFiles.java
@@ -14,6 +14,7 @@ import java.util.TreeMap;
 import javax.imageio.ImageIO;
 
 import org.openstreetmap.josm.Main;
+import org.openstreetmap.josm.tools.ImageProvider;
 
 /**
  * Use this class if you want to cache a lot of files that shouldn't be kept in memory. You can
@@ -167,7 +168,7 @@ public class CacheFiles {
             if(updateModTime) {
                 img.setLastModified(System.currentTimeMillis());
             }
-            return ImageIO.read(img);
+            return ImageProvider.read(img, false, false);
         } catch (Exception e) {
             Main.warn(e);
         }

src/org/openstreetmap/josm/io/imagery/Grabber.java
@@ -43,11 +43,13 @@ public abstract class Grabber implements Runnable {
             if (request.isPrecacheOnly()) {
                 if (!layer.cache.hasExactMatch(Main.getProjection(), request.getPixelPerDegree(), b.minEast, b.minNorth)) {
                     attempt(request);
+                } else if (Main.isDebugEnabled()) {
+                    Main.debug("Ignoring "+request+" (precache only + exact match)");
                 }
-            } else {
-                if(!loadFromCache(request)){
-                    attempt(request);
-                }
+            } else if (!loadFromCache(request)){
+                attempt(request);
+            } else if (Main.isDebugEnabled()) {
+                Main.debug("Ignoring "+request+" (loaded from cache)");
             }
             layer.finishRequest(request);
         }
@@ -69,7 +71,7 @@ public abstract class Grabber implements Runnable {
                 } catch (InterruptedException e1) {
                     Main.debug("InterruptedException in "+getClass().getSimpleName()+" during WMS request");
                 }
-                if(i == maxTries) {
+                if (i == maxTries) {
                     Main.error(e);
                     request.finish(State.FAILED, null);
                 }

src/org/openstreetmap/josm/io/imagery/HTMLGrabber.java
@@ -11,12 +11,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-import javax.imageio.ImageIO;
-
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.data.preferences.StringProperty;
 import org.openstreetmap.josm.gui.MapView;
 import org.openstreetmap.josm.gui.layer.WMSLayer;
+import org.openstreetmap.josm.tools.ImageProvider;
 import org.openstreetmap.josm.tools.Utils;
 
 public class HTMLGrabber extends WMSGrabber {
@@ -50,7 +49,7 @@ public class HTMLGrabber extends WMSGrabber {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         Utils.copyStream(browser.getInputStream(), baos);
         ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
-        BufferedImage img = layer.normalizeImage(ImageIO.read(bais));
+        BufferedImage img = layer.normalizeImage(ImageProvider.read(bais, true, WMSLayer.PROP_ALPHA_CHANNEL.get()));
         bais.reset();
         layer.cache.saveToCache(layer.isOverlapEnabled()?img:null, bais, Main.getProjection(), request.getPixelPerDegree(), b.minEast, b.minNorth);
 

commit 01b7a13053834675289bbddc5eac5950e09bcc75

fix #10030 - wms support broken in r7132

src/org/openstreetmap/josm/tools/ImageProvider.java
@@ -62,6 +62,7 @@ import org.openstreetmap.josm.io.MirroredInputStream;
 import org.openstreetmap.josm.plugins.PluginHandler;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.Attributes;
 import org.xml.sax.EntityResolver;
 import org.xml.sax.InputSource;
@@ -1271,21 +1272,24 @@ public class ImageProvider {
                     if ("javax_imageio_1.0".equals(f)) {
                         Node root = metadata.getAsTree(f);
                         if (root instanceof Element) {
-                            Node item = ((Element)root).getElementsByTagName("TransparentColor").item(0);
-                            if (item instanceof Element) {
-                                String value = ((Element)item).getAttribute("value");
-                                String[] s = value.split(" ");
-                                if (s.length == 3) {
-                                    int[] rgb = new int[3];
-                                    try {
-                                        for (int i = 0; i<3; i++) {
-                                            rgb[i] = Integer.parseInt(s[i]);
+                            NodeList list = ((Element)root).getElementsByTagName("TransparentColor");
+                            if (list.getLength() > 0) {
+                                Node item = list.item(0);
+                                if (item instanceof Element) {
+                                    String value = ((Element)item).getAttribute("value");
+                                    String[] s = value.split(" ");
+                                    if (s.length == 3) {
+                                        int[] rgb = new int[3];
+                                        try {
+                                            for (int i = 0; i<3; i++) {
+                                                rgb[i] = Integer.parseInt(s[i]);
+                                            }
+                                            return new Color(rgb[0], rgb[1], rgb[2]);
+                                        } catch (IllegalArgumentException e) {
+                                            Main.error(e);
                                         }
-                                        return new Color(rgb[0], rgb[1], rgb[2]);
-                                    } catch (IllegalArgumentException e) {
-                                        Main.error(e);
                                     }
-                                }
+                            }
                             }
                         }
                         break;

test/unit/org/openstreetmap/josm/tools/ImageProviderTest.java
@@ -28,4 +28,14 @@ public class ImageProviderTest {
         assertThat(ImageProvider.read(file, false, false).getTransparency(), is(Transparency.OPAQUE));
         assertThat(ImageProvider.read(file, true, false).getTransparency(), is(Transparency.OPAQUE));
     }
+
+    /**
+     * Non-regression test for ticket <a href="https://josm.openstreetmap.de/ticket/10030">#10030</a>
+     * @throws IOException if an error occurs during reading
+     */
+    @Test
+    public void testTicket10030() throws IOException {
+        File file = new File(TestUtils.getTestDataRoot()+"regress/10030/tile.jpg");
+        ImageProvider.read(file, true, true);
+    }
 }