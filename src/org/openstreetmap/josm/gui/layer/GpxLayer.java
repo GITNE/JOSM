GITNE/JOSM

commit 2729da084eab3c68dcc7cfeaa8f8eaee7e7d9bc4

fix #10121 - Add a new look-and-feel preference to display ISO 8601 dates globally
git-svn-id: https://josm.openstreetmap.de/svn/trunk@7299 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/layer/GpxLayer.java
@@ -56,6 +56,7 @@ import org.openstreetmap.josm.gui.widgets.HtmlPanel;
 import org.openstreetmap.josm.io.GpxImporter;
 import org.openstreetmap.josm.tools.ImageProvider;
 import org.openstreetmap.josm.tools.Utils;
+import org.openstreetmap.josm.tools.date.DateUtils;
 
 public class GpxLayer extends Layer {
 
@@ -141,22 +142,24 @@ public class GpxLayer extends Layer {
 
 
     /**
-     * returns a human readable string that shows the timespan of the given track
+     * Returns a human readable string that shows the timespan of the given track
+     * @param trk The GPX track for which timespan is displayed
+     * @return The timespan as a string
      */
     public static String getTimespanForTrack(GpxTrack trk) {
         Date[] bounds = getMinMaxTimeForTrack(trk);
         String ts = "";
         if (bounds != null) {
-            DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT);
+            DateFormat df = DateUtils.getDateFormat(DateFormat.SHORT);
             String earliestDate = df.format(bounds[0]);
             String latestDate = df.format(bounds[1]);
 
             if (earliestDate.equals(latestDate)) {
-                DateFormat tf = DateFormat.getTimeInstance(DateFormat.SHORT);
+                DateFormat tf = DateUtils.getTimeFormat(DateFormat.SHORT);
                 ts += earliestDate + " ";
                 ts += tf.format(bounds[0]) + " - " + tf.format(bounds[1]);
             } else {
-                DateFormat dtf = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT);
+                DateFormat dtf = DateUtils.getDateTimeFormat(DateFormat.SHORT, DateFormat.SHORT);
                 ts += dtf.format(bounds[0]) + " - " + dtf.format(bounds[1]);
             }
 

src/org/openstreetmap/josm/gui/layer/geoimage/CorrelateGpxWithImages.java
@@ -23,6 +23,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -75,7 +76,8 @@ import org.openstreetmap.josm.io.GpxReader;
 import org.openstreetmap.josm.tools.ExifReader;
 import org.openstreetmap.josm.tools.GBC;
 import org.openstreetmap.josm.tools.ImageProvider;
-import org.openstreetmap.josm.tools.PrimaryDateParser;
+import org.openstreetmap.josm.tools.date.DateUtils;
+import org.openstreetmap.josm.tools.date.PrimaryDateParser;
 import org.xml.sax.SAXException;
 
 /**
@@ -235,7 +237,7 @@ public class CorrelateGpxWithImages extends AbstractAction {
 
         @Override
         public void actionPerformed(ActionEvent arg0) {
-            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+            SimpleDateFormat dateFormat = (SimpleDateFormat) DateUtils.getDateTimeFormat(DateFormat.SHORT, DateFormat.SHORT);
 
             panel = new JPanel();
             panel.setLayout(new BorderLayout());
@@ -284,7 +286,7 @@ public class CorrelateGpxWithImages extends AbstractAction {
 
             gc.gridx = 2;
             gc.weightx = 0.2;
-            panelTf.add(new JLabel(tr(" [dd/mm/yyyy hh:mm:ss]")), gc);
+            panelTf.add(new JLabel(" ["+dateFormat.toLocalizedPattern()+"]"), gc);
 
             gc.gridx = 0;
             gc.gridy = 2;
@@ -359,8 +361,9 @@ public class CorrelateGpxWithImages extends AbstractAction {
                     imgDisp.setImage(yLayer.data.get(index).getFile(), orientation);
                     Date date = yLayer.data.get(index).getExifTime();
                     if (date != null) {
-                        lbExifTime.setText(new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(date));
-                        tfGpsTime.setText(new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(date));
+                        DateFormat df = DateUtils.getDateTimeFormat(DateFormat.SHORT, DateFormat.SHORT);
+                        lbExifTime.setText(df.format(date));
+                        tfGpsTime.setText(df.format(date));
                         tfGpsTime.setCaretPosition(tfGpsTime.getText().length());
                         tfGpsTime.setEnabled(true);
                         tfGpsTime.requestFocus();
@@ -370,7 +373,6 @@ public class CorrelateGpxWithImages extends AbstractAction {
                         tfGpsTime.setEnabled(false);
                     }
                 }
-
             });
             panelLst.add(new JScrollPane(imgList), BorderLayout.CENTER);
 
@@ -379,7 +381,8 @@ public class CorrelateGpxWithImages extends AbstractAction {
 
                 @Override
                 public void actionPerformed(ActionEvent ae) {
-                    JFileChooser fc = DiskAccessAction.createAndOpenFileChooser(true, false, null, JpegFileFilter.getInstance(), JFileChooser.FILES_ONLY, "geoimage.lastdirectory");
+                    JFileChooser fc = DiskAccessAction.createAndOpenFileChooser(true, false, null, JpegFileFilter.getInstance(),
+                            JFileChooser.FILES_ONLY, "geoimage.lastdirectory");
                     if (fc == null)
                         return;
                     File sel = fc.getSelectedFile();
@@ -399,8 +402,8 @@ public class CorrelateGpxWithImages extends AbstractAction {
                         Main.warn(e);
                     }
                     if (date != null) {
-                        lbExifTime.setText(new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(date));
-                        tfGpsTime.setText(new SimpleDateFormat("dd/MM/yyyy ").format(date));
+                        lbExifTime.setText(DateUtils.getDateTimeFormat(DateFormat.SHORT, DateFormat.SHORT).format(date));
+                        tfGpsTime.setText(DateUtils.getDateFormat(DateFormat.SHORT).format(date)+" ");
                         tfGpsTime.setEnabled(true);
                     } else {
                         lbExifTime.setText(tr("No date"));
@@ -928,7 +931,8 @@ public class CorrelateGpxWithImages extends AbstractAction {
                     tfTimezone.getDocument().addDocumentListener(statusBarUpdater);
                     tfOffset.getDocument().addDocumentListener(statusBarUpdater);
 
-                    lblMatches.setText(statusBarText.getText() + "<br>" + trn("(Time difference of {0} day)", "Time difference of {0} days", Math.abs(dayOffset), Math.abs(dayOffset)));
+                    lblMatches.setText(statusBarText.getText() + "<br>" + trn("(Time difference of {0} day)",
+                            "Time difference of {0} days", Math.abs(dayOffset), Math.abs(dayOffset)));
 
                     statusBarUpdater.updateStatusBar();
                     yLayer.updateBufferAndRepaint();

src/org/openstreetmap/josm/gui/layer/geoimage/ImageViewerDialog.java
@@ -26,6 +26,7 @@ import org.openstreetmap.josm.gui.dialogs.DialogsPanel.Action;
 import org.openstreetmap.josm.gui.dialogs.ToggleDialog;
 import org.openstreetmap.josm.tools.ImageProvider;
 import org.openstreetmap.josm.tools.Shortcut;
+import org.openstreetmap.josm.tools.date.DateUtils;
 
 public final class ImageViewerDialog extends ToggleDialog {
 
@@ -53,7 +54,7 @@ public final class ImageViewerDialog extends ToggleDialog {
 
     public static ImageViewerDialog getInstance() {
         if (dialog == null)
-            throw new AssertionError("a new instance needs to be created first"); 
+            throw new AssertionError("a new instance needs to be created first");
         return dialog;
     }
 
@@ -247,7 +248,7 @@ public final class ImageViewerDialog extends ToggleDialog {
 
         if (entry != null) {
             if (imageChanged) {
-                // Set only if the image is new to preserve zoom and position if the same image is redisplayed 
+                // Set only if the image is new to preserve zoom and position if the same image is redisplayed
                 // (e.g. to update the OSD).
                 imgDisplay.setImage(entry.getFile(), entry.getExifOrientation());
             }
@@ -262,7 +263,7 @@ public final class ImageViewerDialog extends ToggleDialog {
             if (entry.getExifImgDir() != null) {
                 osd.append(tr("\nDirection {0}\u00b0", Math.round(entry.getExifImgDir())));
             }
-            DateFormat dtf = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT);
+            DateFormat dtf = DateUtils.getDateTimeFormat(DateFormat.SHORT, DateFormat.SHORT);
             if (entry.hasExifTime()) {
                 osd.append(tr("\nEXIF time: {0}", dtf.format(entry.getExifTime())));
             }

src/org/openstreetmap/josm/actions/search/SearchCompiler.java
@@ -26,9 +26,9 @@ import org.openstreetmap.josm.data.osm.OsmUtils;
 import org.openstreetmap.josm.data.osm.Relation;
 import org.openstreetmap.josm.data.osm.RelationMember;
 import org.openstreetmap.josm.data.osm.Way;
-import org.openstreetmap.josm.tools.DateUtils;
 import org.openstreetmap.josm.tools.Geometry;
 import org.openstreetmap.josm.tools.Predicate;
+import org.openstreetmap.josm.tools.date.DateUtils;
 
 /**
  Implements a google-like search.

src/org/openstreetmap/josm/data/gpx/WayPoint.java
@@ -11,7 +11,7 @@ import org.openstreetmap.josm.actions.search.SearchCompiler.Match;
 import org.openstreetmap.josm.data.coor.EastNorth;
 import org.openstreetmap.josm.data.coor.LatLon;
 import org.openstreetmap.josm.data.projection.Projections;
-import org.openstreetmap.josm.tools.PrimaryDateParser;
+import org.openstreetmap.josm.tools.date.PrimaryDateParser;
 import org.openstreetmap.josm.tools.template_engine.TemplateEngineDataProvider;
 
 public class WayPoint extends WithAttributes implements Comparable<WayPoint>, TemplateEngineDataProvider {

src/org/openstreetmap/josm/data/imagery/WmsCache.java
@@ -13,7 +13,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.lang.ref.SoftReference;
 import java.net.URLConnection;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
@@ -44,6 +43,7 @@ import org.openstreetmap.josm.data.projection.Projection;
 import org.openstreetmap.josm.gui.layer.WMSLayer;
 import org.openstreetmap.josm.tools.ImageProvider;
 import org.openstreetmap.josm.tools.Utils;
+import org.openstreetmap.josm.tools.date.DateUtils;
 
 public class WmsCache {
     //TODO Property for maximum cache size
@@ -557,7 +557,7 @@ public class WmsCache {
     }
 
     public static String printDate(Calendar c) {
-        return (new SimpleDateFormat("yyyy-MM-dd")).format(c.getTime());
+        return DateUtils.newIsoDateFormat().format(c.getTime());
     }
 
     private boolean isInsideAreaToCache(CacheEntry cacheEntry) {

src/org/openstreetmap/josm/gui/dialogs/InspectPrimitiveDialog.java
@@ -45,10 +45,10 @@ import org.openstreetmap.josm.gui.mappaint.StyleSource;
 import org.openstreetmap.josm.gui.mappaint.mapcss.MapCSSStyleSource;
 import org.openstreetmap.josm.gui.mappaint.xml.XmlStyleSource;
 import org.openstreetmap.josm.gui.widgets.JosmTextArea;
-import org.openstreetmap.josm.tools.DateUtils;
 import org.openstreetmap.josm.tools.GBC;
 import org.openstreetmap.josm.tools.Geometry;
 import org.openstreetmap.josm.tools.WindowGeometry;
+import org.openstreetmap.josm.tools.date.DateUtils;
 
 /**
  * Panel to inspect one or more OsmPrimitives.

src/org/openstreetmap/josm/gui/dialogs/ToggleDialog.java
@@ -89,7 +89,7 @@ public class ToggleDialog extends JPanel implements ShowHideButtonListener, Help
     }
 
     /**
-     * Property to enable dyanmic buttons globally.
+     * Property to enable dynamic buttons globally.
      * @since 6752
      */
     public static final BooleanProperty PROP_DYNAMIC_BUTTONS = new BooleanProperty("dialog.dynamic.buttons", false);

commit 0a2409a4439155c7d41aaca0410620bd3add065f

fix #10283 - wrong time format in GPX/images layer leading to wrong correlation (regression from r7299)

src/org/openstreetmap/josm/gui/layer/GpxLayer.java
@@ -21,16 +21,14 @@ import javax.swing.Action;
 import javax.swing.Icon;
 import javax.swing.JScrollPane;
 import javax.swing.SwingUtilities;
-import org.openstreetmap.josm.Main;
 
+import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.actions.RenameLayerAction;
 import org.openstreetmap.josm.actions.SaveActionBase;
 import org.openstreetmap.josm.data.Bounds;
 import org.openstreetmap.josm.data.gpx.GpxConstants;
 import org.openstreetmap.josm.data.gpx.GpxData;
-import org.openstreetmap.josm.data.gpx.GpxRoute;
 import org.openstreetmap.josm.data.gpx.GpxTrack;
-import org.openstreetmap.josm.data.gpx.GpxTrackSegment;
 import org.openstreetmap.josm.data.gpx.WayPoint;
 import org.openstreetmap.josm.data.osm.visitor.BoundingXYVisitor;
 import org.openstreetmap.josm.data.projection.Projection;
@@ -63,7 +61,7 @@ public class GpxLayer extends Layer {
     private int lastUpdateCount;
 
     private final GpxDrawHelper drawHelper;
-    
+
     public GpxLayer(GpxData d) {
         super((String) d.attr.get("name"));
         data = d;
@@ -86,7 +84,7 @@ public class GpxLayer extends Layer {
     public Color getColor(boolean ignoreCustom) {
         return drawHelper.getColor(getName(), ignoreCustom);
     }
-    
+
     /**
      * Returns a human readable string that shows the timespan of the given track
      * @param trk The GPX track for which timespan is displayed
@@ -105,7 +103,7 @@ public class GpxLayer extends Layer {
                 ts += earliestDate + " ";
                 ts += tf.format(bounds[0]) + " - " + tf.format(bounds[1]);
             } else {
-                DateFormat dtf = DateUtils.getDateTimeFormat(DateFormat.SHORT, DateFormat.SHORT);
+                DateFormat dtf = DateUtils.getDateTimeFormat(DateFormat.SHORT, DateFormat.MEDIUM);
                 ts += dtf.format(bounds[0]) + " - " + dtf.format(bounds[1]);
             }
 
@@ -289,16 +287,16 @@ public class GpxLayer extends Layer {
                 drawHelper.drawColorBar(g, mv);
             }
         }
-        
+
     }
-    
+
     private LinkedList<WayPoint> listVisibleSegments(Bounds box) {
         WayPoint last = null;
         LinkedList<WayPoint> visibleSegments = new LinkedList<>();
-        
+
         ensureTrackVisibilityLength();
         for (Collection<WayPoint> segment : data.getLinesIterable(trackVisibility)) {
-            
+
             for(WayPoint pt : segment)
             {
                 Bounds b = new Bounds(pt.getCoor());

src/org/openstreetmap/josm/gui/layer/geoimage/CorrelateGpxWithImages.java
@@ -237,7 +237,7 @@ public class CorrelateGpxWithImages extends AbstractAction {
 
         @Override
         public void actionPerformed(ActionEvent arg0) {
-            SimpleDateFormat dateFormat = (SimpleDateFormat) DateUtils.getDateTimeFormat(DateFormat.SHORT, DateFormat.SHORT);
+            SimpleDateFormat dateFormat = (SimpleDateFormat) DateUtils.getDateTimeFormat(DateFormat.SHORT, DateFormat.MEDIUM);
 
             panel = new JPanel();
             panel.setLayout(new BorderLayout());
@@ -361,7 +361,7 @@ public class CorrelateGpxWithImages extends AbstractAction {
                     imgDisp.setImage(yLayer.data.get(index).getFile(), orientation);
                     Date date = yLayer.data.get(index).getExifTime();
                     if (date != null) {
-                        DateFormat df = DateUtils.getDateTimeFormat(DateFormat.SHORT, DateFormat.SHORT);
+                        DateFormat df = DateUtils.getDateTimeFormat(DateFormat.SHORT, DateFormat.MEDIUM);
                         lbExifTime.setText(df.format(date));
                         tfGpsTime.setText(df.format(date));
                         tfGpsTime.setCaretPosition(tfGpsTime.getText().length());
@@ -402,7 +402,7 @@ public class CorrelateGpxWithImages extends AbstractAction {
                         Main.warn(e);
                     }
                     if (date != null) {
-                        lbExifTime.setText(DateUtils.getDateTimeFormat(DateFormat.SHORT, DateFormat.SHORT).format(date));
+                        lbExifTime.setText(DateUtils.getDateTimeFormat(DateFormat.SHORT, DateFormat.MEDIUM).format(date));
                         tfGpsTime.setText(DateUtils.getDateFormat(DateFormat.SHORT).format(date)+" ");
                         tfGpsTime.setEnabled(true);
                     } else {

src/org/openstreetmap/josm/gui/layer/geoimage/ImageViewerDialog.java
@@ -263,7 +263,7 @@ public final class ImageViewerDialog extends ToggleDialog {
             if (entry.getExifImgDir() != null) {
                 osd.append(tr("\nDirection {0}\u00b0", Math.round(entry.getExifImgDir())));
             }
-            DateFormat dtf = DateUtils.getDateTimeFormat(DateFormat.SHORT, DateFormat.SHORT);
+            DateFormat dtf = DateUtils.getDateTimeFormat(DateFormat.SHORT, DateFormat.MEDIUM);
             if (entry.hasExifTime()) {
                 osd.append(tr("\nEXIF time: {0}", dtf.format(entry.getExifTime())));
             }