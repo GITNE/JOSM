GITNE/JOSM

commit c0a3c01c6d747420c2da9970b7a10cb8f353e648

fix #12160 - Combobox List Entries with Empty Value – `display_value` and `short_description` not shown
git-svn-id: http://josm.openstreetmap.de/svn/trunk@9465 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/tagging/presets/items/ComboMultiSelect.java
@@ -200,13 +200,10 @@ public abstract class ComboMultiSelect extends KeyedItem {
 
         public String getListDisplay() {
             if (value.equals(DIFFERENT))
-                return "<b>"+DIFFERENT.replaceAll("<", "&lt;").replaceAll(">", "&gt;")+"</b>";
-
-            if (value.isEmpty())
-                return "&nbsp;";
+                return "<b>" + Utils.escapeReservedCharactersHTML(DIFFERENT) + "</b>";
 
             final StringBuilder res = new StringBuilder("<b>");
-            res.append(getDisplayValue(true).replaceAll("<", "&lt;").replaceAll(">", "&gt;"))
+            res.append(Utils.escapeReservedCharactersHTML(getDisplayValue(true)))
                .append("</b>");
             if (getShortDescription(true) != null) {
                 // wrap in table to restrict the text width

src/org/openstreetmap/josm/gui/tagging/presets/items/Combo.java
@@ -38,7 +38,9 @@ public class Combo extends ComboMultiSelect {
         if (def != null && !lhm.containsKey(def)) {
             lhm.put(def, new PresetListEntry(def));
         }
-        lhm.put("", new PresetListEntry(""));
+        if (!lhm.containsKey("")) {
+            lhm.put("", new PresetListEntry(""));
+        }
 
         combo = new JosmComboBox<>(lhm.values().toArray(new PresetListEntry[0]));
         component = combo;

commit 42ad55c4a528221d86ad0c06ecf26c5511c5df63

fix #12416 - see #12160 - fix unnecessary use of scrollbar in tagging presets combos (regression from r9465)

src/org/openstreetmap/josm/gui/tagging/presets/items/ComboMultiSelect.java
@@ -249,13 +249,17 @@ public abstract class ComboMultiSelect extends KeyedItem {
             if (value.equals(DIFFERENT))
                 return "<b>" + Utils.escapeReservedCharactersHTML(DIFFERENT) + "</b>";
 
-            final StringBuilder res = new StringBuilder("<b>");
-            res.append(Utils.escapeReservedCharactersHTML(getDisplayValue(true)))
-               .append("</b>");
-            if (getShortDescription(true) != null) {
+            String displayValue = Utils.escapeReservedCharactersHTML(getDisplayValue(true));
+            String shortDescription = getShortDescription(true);
+
+            if (displayValue.isEmpty() && (shortDescription == null || shortDescription.isEmpty()))
+                return "&nbsp;";
+
+            final StringBuilder res = new StringBuilder("<b>").append(displayValue).append("</b>");
+            if (shortDescription != null) {
                 // wrap in table to restrict the text width
                 res.append("<div style=\"width:300px; padding:0 0 5px 5px\">")
-                   .append(getShortDescription(true))
+                   .append(shortDescription)
                    .append("</div>");
             }
             return res.toString();
@@ -294,9 +298,10 @@ public abstract class ComboMultiSelect extends KeyedItem {
         // toString is mainly used to initialize the Editor
         @Override
         public String toString() {
-            if (value.equals(DIFFERENT))
+            if (DIFFERENT.equals(value))
                 return DIFFERENT;
-            return getDisplayValue(true).replaceAll("<.*>", ""); // remove additional markup, e.g. <br>
+            String displayValue = getDisplayValue(true);
+            return displayValue != null ? displayValue.replaceAll("<.*>", "") : null; // remove additional markup, e.g. <br>
         }
 
         @Override

test/unit/org/openstreetmap/josm/gui/tagging/presets/items/ComboMultiSelectTest.java
@@ -1,0 +1,31 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.gui.tagging.presets.items;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openstreetmap.josm.JOSMFixture;
+import org.openstreetmap.josm.gui.tagging.presets.items.ComboMultiSelect.PresetListEntry;
+
+/**
+ * Unit tests of {@link ComboMultiSelect} class.
+ */
+public class ComboMultiSelectTest {
+
+    /**
+     * Setup test.
+     */
+    @BeforeClass
+    public static void setUp() {
+        JOSMFixture.createUnitTestFixture().init();
+    }
+
+    /**
+     * Non-regression test for ticket <a href="https://josm.openstreetmap.de/ticket/12416">#12416</a>.
+     */
+    @Test
+    public void testTicket12416() {
+        assertEquals("&nbsp;", new PresetListEntry("").getListDisplay());
+    }
+}