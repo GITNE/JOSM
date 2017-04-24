GITNE/JOSM

commit 614590605489cfd13229cd39037d35ed2c80d9c8

add unit test for `Main.postConstructorProcessCmdLine`
git-svn-id: https://josm.openstreetmap.de/svn/trunk@11986 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/Main.java
@@ -17,6 +17,7 @@ import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -825,16 +826,29 @@ public abstract class Main {
         }
     }
 
+    /**
+     * Handle command line instructions after GUI has been initialized.
+     * @param args program arguments
+     */
     protected static void postConstructorProcessCmdLine(ProgramArguments args) {
+        List<Future<?>> tasks = new ArrayList<>();
         List<File> fileList = new ArrayList<>();
         for (String s : args.get(Option.DOWNLOAD)) {
-            DownloadParamType.paramType(s).download(s, fileList);
+            tasks.addAll(DownloadParamType.paramType(s).download(s, fileList));
         }
         if (!fileList.isEmpty()) {
-            OpenFileAction.openFiles(fileList, true);
+            tasks.add(OpenFileAction.openFiles(fileList, true));
         }
         for (String s : args.get(Option.DOWNLOADGPS)) {
-            DownloadParamType.paramType(s).downloadGps(s);
+            tasks.addAll(DownloadParamType.paramType(s).downloadGps(s));
+        }
+        // Make sure all download tasks complete before handling selection arguments
+        for (Future<?> task : tasks) {
+            try {
+                task.get();
+            } catch (InterruptedException | ExecutionException e) {
+                error(e);
+            }
         }
         for (String s : args.get(Option.SELECTION)) {
             SearchAction.search(s, SearchAction.SearchMode.add);
@@ -867,6 +881,9 @@ public abstract class Main {
         return false;
     }
 
+    /**
+     * Shutdown JOSM.
+     */
     protected void shutdown() {
         if (!GraphicsEnvironment.isHeadless()) {
             worker.shutdown();
@@ -896,12 +913,12 @@ public abstract class Main {
     enum DownloadParamType {
         httpUrl {
             @Override
-            void download(String s, Collection<File> fileList) {
-                new OpenLocationAction().openUrl(false, s);
+            List<Future<?>> download(String s, Collection<File> fileList) {
+                return new OpenLocationAction().openUrl(false, s);
             }
 
             @Override
-            void downloadGps(String s) {
+            List<Future<?>> downloadGps(String s) {
                 final Bounds b = OsmUrlToBounds.parse(s);
                 if (b == null) {
                     JOptionPane.showMessageDialog(
@@ -910,13 +927,13 @@ public abstract class Main {
                             tr("Warning"),
                             JOptionPane.WARNING_MESSAGE
                     );
-                    return;
+                    return Collections.emptyList();
                 }
-                downloadFromParamBounds(true, b);
+                return downloadFromParamBounds(true, b);
             }
         }, fileUrl {
             @Override
-            void download(String s, Collection<File> fileList) {
+            List<Future<?>> download(String s, Collection<File> fileList) {
                 File f = null;
                 try {
                     f = new File(new URI(s));
@@ -932,6 +949,7 @@ public abstract class Main {
                 if (f != null) {
                     fileList.add(f);
                 }
+                return Collections.emptyList();
             }
         }, bounds {
 
@@ -939,31 +957,33 @@ public abstract class Main {
              * Download area specified on the command line as bounds string.
              * @param rawGps Flag to download raw GPS tracks
              * @param s The bounds parameter
+             * @return the complete download task (including post-download handler), or {@code null}
              */
-            private void downloadFromParamBounds(final boolean rawGps, String s) {
+            private List<Future<?>> downloadFromParamBounds(final boolean rawGps, String s) {
                 final StringTokenizer st = new StringTokenizer(s, ",");
                 if (st.countTokens() == 4) {
-                    Bounds b = new Bounds(
+                    return Main.downloadFromParamBounds(rawGps, new Bounds(
                             new LatLon(Double.parseDouble(st.nextToken()), Double.parseDouble(st.nextToken())),
                             new LatLon(Double.parseDouble(st.nextToken()), Double.parseDouble(st.nextToken()))
-                    );
-                    Main.downloadFromParamBounds(rawGps, b);
+                    ));
                 }
+                return Collections.emptyList();
             }
 
             @Override
-            void download(String param, Collection<File> fileList) {
-                downloadFromParamBounds(false, param);
+            List<Future<?>> download(String param, Collection<File> fileList) {
+                return downloadFromParamBounds(false, param);
             }
 
             @Override
-            void downloadGps(String param) {
-                downloadFromParamBounds(true, param);
+            List<Future<?>> downloadGps(String param) {
+                return downloadFromParamBounds(true, param);
             }
         }, fileName {
             @Override
-            void download(String s, Collection<File> fileList) {
+            List<Future<?>> download(String s, Collection<File> fileList) {
                 fileList.add(new File(s));
+                return Collections.emptyList();
             }
         };
 
@@ -971,20 +991,23 @@ public abstract class Main {
          * Performs the download
          * @param param represents the object to be downloaded
          * @param fileList files which shall be opened, should be added to this collection
+         * @return the download task, or {@code null}
          */
-        abstract void download(String param, Collection<File> fileList);
+        abstract List<Future<?>> download(String param, Collection<File> fileList);
 
         /**
          * Performs the GPS download
          * @param param represents the object to be downloaded
+         * @return the download task, or {@code null}
          */
-        void downloadGps(String param) {
+        List<Future<?>> downloadGps(String param) {
             JOptionPane.showMessageDialog(
                     Main.parent,
                     tr("Parameter \"downloadgps\" does not accept file names or file URLs"),
                     tr("Warning"),
                     JOptionPane.WARNING_MESSAGE
             );
+            return Collections.emptyList();
         }
 
         /**
@@ -1007,13 +1030,14 @@ public abstract class Main {
      * Download area specified as Bounds value.
      * @param rawGps Flag to download raw GPS tracks
      * @param b The bounds value
+     * @return the complete download task (including post-download handler)
      */
-    private static void downloadFromParamBounds(final boolean rawGps, Bounds b) {
+    private static List<Future<?>> downloadFromParamBounds(final boolean rawGps, Bounds b) {
         DownloadTask task = rawGps ? new DownloadGpsTask() : new DownloadOsmTask();
         // asynchronously launch the download task ...
         Future<?> future = task.download(true, b, null);
         // ... and the continuation when the download is finished (this will wait for the download to finish)
-        Main.worker.execute(new PostDownloadHandler(task, future));
+        return Collections.singletonList(Main.worker.submit(new PostDownloadHandler(task, future)));
     }
 
     /**

test/unit/org/openstreetmap/josm/MainTest.java
@@ -2,17 +2,22 @@
 package org.openstreetmap.josm;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Collection;
+import java.util.List;
 
 import javax.swing.UIManager;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.openstreetmap.josm.Main.DownloadParamType;
+import org.openstreetmap.josm.data.osm.DataSet;
+import org.openstreetmap.josm.gui.ProgramArguments;
+import org.openstreetmap.josm.gui.layer.GpxLayer;
 import org.openstreetmap.josm.testutils.JOSMTestRules;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
@@ -27,7 +32,7 @@ public class MainTest {
      */
     @Rule
     @SuppressFBWarnings(value = "URF_UNREAD_PUBLIC_OR_PROTECTED_FIELD")
-    public JOSMTestRules test = new JOSMTestRules().platform();
+    public JOSMTestRules test = new JOSMTestRules().platform().devAPI();
 
     /**
      * Unit test of {@link DownloadParamType#paramType} method.
@@ -79,5 +84,42 @@ public class MainTest {
         assertNotNull(Main.getProjection());
         assertEquals(Main.pref.get("laf", Main.platform.getDefaultStyle()), UIManager.getLookAndFeel().getClass().getCanonicalName());
         assertNotNull(Main.toolbar);
+    }
+
+    /**
+     * Unit test of {@link Main#postConstructorProcessCmdLine} - empty case.
+     */
+    @Test
+    public void testPostConstructorProcessCmdLineEmpty() {
+        // Check the method accepts no arguments
+        Main.postConstructorProcessCmdLine(new ProgramArguments(new String[0]));
+    }
+
+    /**
+     * Unit test of {@link Main#postConstructorProcessCmdLine} - nominal cases.
+     * This test assumes the DEV API contains nodes around 0,0 and GPX tracks around London
+     */
+    @Test
+    public void testPostConstructorProcessCmdLineNominal() {
+        assertNull(Main.getLayerManager().getEditDataSet());
+        Main.postConstructorProcessCmdLine(new ProgramArguments(new String[]{
+                "--download=0.01,0.01,0.05,0.05",
+                "--downloadgps=51.35,-0.4,51.60,0.2",
+                "--selection=type: node"}));
+        DataSet ds = Main.getLayerManager().getEditDataSet();
+        assertNotNull(ds);
+        assertFalse(ds.getSelected().isEmpty());
+        Main.getLayerManager().removeLayer(Main.getLayerManager().getEditLayer());
+        List<GpxLayer> gpxLayers = Main.getLayerManager().getLayersOfType(GpxLayer.class);
+        assertEquals(1, gpxLayers.size());
+        Main.getLayerManager().removeLayer(gpxLayers.iterator().next());
+    }
+
+    /**
+     * Unit test of {@link DownloadParamType} enum.
+     */
+    @Test
+    public void testEnumDownloadParamType() {
+        TestUtils.superficialEnumCodeCoverage(DownloadParamType.class);
     }
 }

src/org/openstreetmap/josm/actions/OpenFileAction.java
@@ -21,6 +21,7 @@ import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.Future;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
@@ -83,20 +84,24 @@ public class OpenFileAction extends DiskAccessAction {
      * Open a list of files. The complete list will be passed to batch importers.
      * Filenames will not be saved in history.
      * @param fileList A list of files
+     * @return the future task
+     * @since 11986 (return task)
      */
-    public static void openFiles(List<File> fileList) {
-        openFiles(fileList, false);
+    public static Future<?> openFiles(List<File> fileList) {
+        return openFiles(fileList, false);
     }
 
     /**
      * Open a list of files. The complete list will be passed to batch importers.
      * @param fileList A list of files
      * @param recordHistory {@code true} to save filename in history (default: false)
+     * @return the future task
+     * @since 11986 (return task)
      */
-    public static void openFiles(List<File> fileList, boolean recordHistory) {
+    public static Future<?> openFiles(List<File> fileList, boolean recordHistory) {
         OpenFileTask task = new OpenFileTask(fileList, null);
         task.setRecordHistory(recordHistory);
-        Main.worker.submit(task);
+        return Main.worker.submit(task);
     }
 
     /**

src/org/openstreetmap/josm/actions/OpenLocationAction.java
@@ -56,6 +56,9 @@ public class OpenLocationAction extends JosmAction {
      * true if the URL needs to be opened in a new layer, false otherwise
      */
     private static final BooleanProperty USE_NEW_LAYER = new BooleanProperty("download.newlayer", false);
+    /**
+     * the list of download tasks
+     */
     protected final transient List<Class<? extends DownloadTask>> downloadTasks;
 
     static class WhichTasksToPerformDialog extends ExtendedDialog {
@@ -199,44 +202,44 @@ public class OpenLocationAction extends JosmAction {
      * Open the given URL.
      * @param newLayer true if the URL needs to be opened in a new layer, false otherwise
      * @param url The URL to open
+     * @return the list of tasks that have been started successfully (can be empty).
+     * @since 11986 (return type)
      */
-    public void openUrl(boolean newLayer, String url) {
-        realOpenUrl(newLayer, url);
+    public List<Future<?>> openUrl(boolean newLayer, String url) {
+        return realOpenUrl(newLayer, url);
     }
 
     /**
      * Open the given URL. This class checks the {@link #USE_NEW_LAYER} preference to check if a new layer should be used.
      * @param url The URL to open
-     * @return <code>true</code> if at least one task was started successfully.
-     * @since 11279
+     * @return the list of tasks that have been started successfully (can be empty).
+     * @since 11986 (return type)
      */
-    public boolean openUrl(String url) {
+    public List<Future<?>> openUrl(String url) {
         return realOpenUrl(USE_NEW_LAYER.get(), url);
     }
 
-    private boolean realOpenUrl(boolean newLayer, String url) {
+    private List<Future<?>> realOpenUrl(boolean newLayer, String url) {
         Collection<DownloadTask> tasks = findDownloadTasks(url, false);
 
         if (tasks.size() > 1) {
             tasks = askWhichTasksToLoad(tasks);
         } else if (tasks.isEmpty()) {
             warnNoSuitableTasks(url);
-            return false;
+            return Collections.emptyList();
         }
 
         PleaseWaitProgressMonitor monitor = new PleaseWaitProgressMonitor(tr("Download Data"));
 
-        boolean hadAnySuccess = false;
+        List<Future<?>> result = new ArrayList<>();
         for (final DownloadTask task : tasks) {
             try {
-                Future<?> future = task.loadUrl(newLayer, url, monitor);
-                Main.worker.submit(new PostDownloadHandler(task, future));
-                hadAnySuccess = true;
+                result.add(Main.worker.submit(new PostDownloadHandler(task, task.loadUrl(newLayer, url, monitor))));
             } catch (IllegalArgumentException e) {
                 Main.error(e);
             }
         }
-        return hadAnySuccess;
+        return result;
     }
 
     /**

src/org/openstreetmap/josm/actions/downloadtasks/DownloadOsmTask.java
@@ -266,8 +266,7 @@ public class DownloadOsmTask extends AbstractDownloadTask<DataSet> {
                 // or it is not clear which layer to merge to
                 //
                 final OsmDataLayer layer = createNewLayer(newLayerName);
-                if (Main.main != null)
-                    Main.getLayerManager().addLayer(layer, zoomAfterDownload);
+                Main.getLayerManager().addLayer(layer, zoomAfterDownload);
                 return layer;
             }
             return null;

src/org/openstreetmap/josm/actions/search/SearchAction.java
@@ -9,6 +9,7 @@ import static org.openstreetmap.josm.tools.I18n.trn;
 import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GraphicsEnvironment;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
@@ -595,14 +596,18 @@ public class SearchAction extends JosmAction implements ParameterizedAction {
                 } else {
                     msg = null;
                 }
-                Main.map.statusLine.setHelpText(msg);
-                JOptionPane.showMessageDialog(
-                        Main.parent,
-                        msg,
-                        tr("Warning"),
-                        JOptionPane.WARNING_MESSAGE
-                );
-            } else {
+                if (Main.map != null) {
+                    Main.map.statusLine.setHelpText(msg);
+                }
+                if (!GraphicsEnvironment.isHeadless()) {
+                    JOptionPane.showMessageDialog(
+                            Main.parent,
+                            msg,
+                            tr("Warning"),
+                            JOptionPane.WARNING_MESSAGE
+                    );
+                }
+            } else if (Main.map != null) {
                 Main.map.statusLine.setHelpText(tr("Found {0} matches", foundMatches));
             }
         }

src/org/openstreetmap/josm/gui/ProgramArguments.java
@@ -135,7 +135,7 @@ public class ProgramArguments {
             if (opt != null) {
                 addOption(opt, g.getOptarg());
             } else
-                throw new IllegalArgumentException("Invalid option: "+c);
+                throw new IllegalArgumentException("Invalid option: "+ (char) c);
         }
         // positional arguments are a shortcut for the --download ... option
         for (int i = g.getOptind(); i < args.length; ++i) {

src/org/openstreetmap/josm/gui/datatransfer/importers/OsmLinkPaster.java
@@ -50,7 +50,7 @@ public class OsmLinkPaster extends AbstractOsmDataPaster {
         }
 
         String transferData = (String) support.getTransferable().getTransferData(df);
-        if (new NoWarnOpenLocationAction().openUrl(transferData)) {
+        if (!new NoWarnOpenLocationAction().openUrl(transferData).isEmpty()) {
             return true;
         }
 

commit 061b3a0f6ef1e836aa6efd0c727a385afce78e40

fix #14690 - Loading commandline file blocks JOSM (regression from r11986)

src/org/openstreetmap/josm/Main.java
@@ -829,8 +829,9 @@ public abstract class Main {
     /**
      * Handle command line instructions after GUI has been initialized.
      * @param args program arguments
+     * @return the list of submitted tasks
      */
-    protected static void postConstructorProcessCmdLine(ProgramArguments args) {
+    protected static List<Future<?>> postConstructorProcessCmdLine(ProgramArguments args) {
         List<Future<?>> tasks = new ArrayList<>();
         List<File> fileList = new ArrayList<>();
         for (String s : args.get(Option.DOWNLOAD)) {
@@ -842,17 +843,15 @@ public abstract class Main {
         for (String s : args.get(Option.DOWNLOADGPS)) {
             tasks.addAll(DownloadParamType.paramType(s).downloadGps(s));
         }
-        // Make sure all download tasks complete before handling selection arguments
-        for (Future<?> task : tasks) {
-            try {
-                task.get();
-            } catch (InterruptedException | ExecutionException e) {
-                error(e);
-            }
+        final Collection<String> selectionArguments = args.get(Option.SELECTION);
+        if (!selectionArguments.isEmpty()) {
+            tasks.add(Main.worker.submit(() -> {
+                for (String s : selectionArguments) {
+                    SearchAction.search(s, SearchAction.SearchMode.add);
+                }
+            }));
         }
-        for (String s : args.get(Option.SELECTION)) {
-            SearchAction.search(s, SearchAction.SearchMode.add);
-        }
+        return tasks;
     }
 
     /**

test/unit/org/openstreetmap/josm/MainTest.java
@@ -11,6 +11,8 @@ import java.net.MalformedURLException;
 import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 import javax.swing.UIManager;
 
@@ -99,10 +101,16 @@ public class MainTest {
 
     private static void doTestPostConstructorProcessCmdLine(String download, String downloadGps, boolean gpx) {
         assertNull(Main.getLayerManager().getEditDataSet());
-        Main.postConstructorProcessCmdLine(new ProgramArguments(new String[]{
+        for (Future<?> f : Main.postConstructorProcessCmdLine(new ProgramArguments(new String[]{
                 "--download=" + download,
                 "--downloadgps=" + downloadGps,
-                "--selection=type: node"}));
+                "--selection=type: node"}))) {
+            try {
+                f.get();
+            } catch (InterruptedException | ExecutionException e) {
+                Main.error(e);
+            }
+        }
         DataSet ds = Main.getLayerManager().getEditDataSet();
         assertNotNull(ds);
         assertFalse(ds.getSelected().isEmpty());