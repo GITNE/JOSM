GITNE/JOSM

commit 2339bb9fc9a850e4971399621a57fbd74bd6619b

fix #15688 - do not filter selected way nodes in draw mode
git-svn-id: https://josm.openstreetmap.de/svn/trunk@13229 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/actions/mapmode/DrawAction.java
@@ -1312,7 +1312,16 @@ public class DrawAction extends MapMode implements MapViewPaintable, DataSelecti
     @Override
     public Collection<? extends OsmPrimitive> getPreservedPrimitives() {
         DataSet ds = getLayerManager().getEditDataSet();
-        return ds != null ? ds.getSelected() : Collections.emptySet();
+        if (ds != null) {
+            // Preserves selected primitives and selected way nodes
+            Set<OsmPrimitive> result = new HashSet<>();
+            for (Way w : ds.getSelectedWays()) {
+                result.addAll(w.getNodes());
+            }
+            result.addAll(ds.getSelected());
+            return result;
+        }
+        return Collections.emptySet();
     }
 
     @Override

commit 0dd60468c677e44f813ea3bca9875900e1586507

fix #15766, see #15688 - fix performance regression introduced in r13229 when drawing a way of many nodes while the filter dialog is open

src/org/openstreetmap/josm/actions/mapmode/DrawAction.java
@@ -198,9 +198,10 @@ public class DrawAction extends MapMode implements MapViewPaintable, DataSelecti
 
     /**
      * Checks if a map redraw is required and does so if needed. Also updates the status bar.
+     * @param e event, can be null
      * @return true if a repaint is needed
      */
-    private boolean redrawIfRequired() {
+    private boolean redrawIfRequired(Object e) {
         updateStatusLine();
         // repaint required if the helper line is active.
         boolean needsRepaint = DRAW_HELPER_LINE.get() && !wayIsFinished;
@@ -235,9 +236,14 @@ public class DrawAction extends MapMode implements MapViewPaintable, DataSelecti
                 addRemoveSelection(currentDataSet, baseNode, continueFrom);
                 needsRepaint = true;
             } else if (!alt && continueFrom != null && !continueFrom.isSelected()) {
-                currentDataSet.addSelected(continueFrom);
+                addSelection(currentDataSet, continueFrom);
                 needsRepaint = true;
             }
+        }
+
+        if (!needsRepaint && e instanceof SelectionChangeEvent) {
+            SelectionChangeEvent event = (SelectionChangeEvent) e;
+            needsRepaint = !event.getOldSelection().isEmpty() && event.getSelection().isEmpty();
         }
 
         if (needsRepaint && editLayer != null) {
@@ -249,11 +255,41 @@ public class DrawAction extends MapMode implements MapViewPaintable, DataSelecti
     private static void addRemoveSelection(DataSet ds, OsmPrimitive toAdd, OsmPrimitive toRemove) {
         ds.beginUpdate(); // to prevent the selection listener to screw around with the state
         try {
-            ds.addSelected(toAdd);
-            ds.clearSelection(toRemove);
+            addSelection(ds, toAdd);
+            clearSelection(ds, toRemove);
         } finally {
             ds.endUpdate();
         }
+    }
+
+    private static void updatePreservedFlag(OsmPrimitive osm, boolean state) {
+        // Preserves selected primitives and selected way nodes
+        osm.setPreserved(state);
+        if (osm instanceof Way) {
+            for (Node n : ((Way) osm).getNodes()) {
+                n.setPreserved(state);
+            }
+        }
+    }
+
+    private static void setSelection(DataSet ds, Collection<OsmPrimitive> toSet) {
+        toSet.stream().forEach(x -> updatePreservedFlag(x, true));
+        ds.setSelected(toSet);
+    }
+
+    private static void setSelection(DataSet ds, OsmPrimitive toSet) {
+        updatePreservedFlag(toSet, true);
+        ds.setSelected(toSet);
+    }
+
+    private static void addSelection(DataSet ds, OsmPrimitive toAdd) {
+        updatePreservedFlag(toAdd, true);
+        ds.addSelected(toAdd);
+    }
+
+    private static void clearSelection(DataSet ds, OsmPrimitive toRemove) {
+        ds.clearSelection(toRemove);
+        updatePreservedFlag(toRemove, false);
     }
 
     @Override
@@ -302,7 +338,12 @@ public class DrawAction extends MapMode implements MapViewPaintable, DataSelecti
         map.statusLine.getAnglePanel().removeMouseListener(snapHelper.anglePopupListener);
         map.statusLine.activateAnglePanel(false);
 
-        removeHighlighting();
+        DataSet ds = getLayerManager().getEditDataSet();
+        if (ds != null) {
+            ds.getSelected().stream().forEach(x -> updatePreservedFlag(x, false));
+        }
+
+        removeHighlighting(null);
         map.keyDetector.removeKeyListener(this);
         map.keyDetector.removeModifierExListener(this);
     }
@@ -316,7 +357,7 @@ public class DrawAction extends MapMode implements MapViewPaintable, DataSelecti
             return;
         updateKeyModifiersEx(modifiers);
         computeHelperLine();
-        addHighlighting();
+        addHighlighting(null);
     }
 
     @Override
@@ -325,7 +366,7 @@ public class DrawAction extends MapMode implements MapViewPaintable, DataSelecti
             return;
         snapHelper.setFixedMode();
         computeHelperLine();
-        redrawIfRequired();
+        redrawIfRequired(e);
     }
 
     @Override
@@ -338,7 +379,7 @@ public class DrawAction extends MapMode implements MapViewPaintable, DataSelecti
         }
         snapHelper.unFixOrTurnOff();
         computeHelperLine();
-        redrawIfRequired();
+        redrawIfRequired(e);
     }
 
     /**
@@ -350,13 +391,15 @@ public class DrawAction extends MapMode implements MapViewPaintable, DataSelecti
             return;
         // Make sure helper line is computed later (causes deadlock in selection event chain otherwise)
         SwingUtilities.invokeLater(() -> {
+            event.getOldSelection().stream().forEach(x -> updatePreservedFlag(x, false));
+            event.getSelection().stream().forEach(x -> updatePreservedFlag(x, true));
             computeHelperLine();
-            addHighlighting();
+            addHighlighting(event);
         });
     }
 
     private void tryAgain(MouseEvent e) {
-        getLayerManager().getEditDataSet().setSelected();
+        getLayerManager().getEditDataSet().clearSelection();
         mouseReleased(e);
     }
 
@@ -373,7 +416,7 @@ public class DrawAction extends MapMode implements MapViewPaintable, DataSelecti
 
         // Redraw to remove the helper line stub
         computeHelperLine();
-        removeHighlighting();
+        removeHighlighting(null);
     }
 
     @Override
@@ -444,11 +487,11 @@ public class DrawAction extends MapMode implements MapViewPaintable, DataSelecti
                 // (this is just a convenience option so that people don't
                 // have to switch modes)
 
-                ds.setSelected(n);
+                setSelection(ds, n);
                 // If we extend/continue an existing way, select it already now to make it obvious
                 Way continueFrom = getWayForNode(n);
                 if (continueFrom != null) {
-                    ds.addSelected(continueFrom);
+                    addSelection(ds, continueFrom);
                 }
 
                 // The user explicitly selected a node, so let him continue drawing
@@ -626,7 +669,7 @@ public class DrawAction extends MapMode implements MapViewPaintable, DataSelecti
             lastUsedNode = n;
         }
 
-        ds.setSelected(newSelection);
+        setSelection(ds, newSelection);
 
         // "viewport following" mode for tracing long features
         // from aerial imagery or GPS tracks.
@@ -634,7 +677,7 @@ public class DrawAction extends MapMode implements MapViewPaintable, DataSelecti
             mapView.smoothScrollTo(n.getEastNorth());
         }
         computeHelperLine();
-        removeHighlighting();
+        removeHighlighting(e);
     }
 
     private static String getTitle(boolean newNode, Node n, Collection<OsmPrimitive> newSelection, List<Way> reuseWays,
@@ -729,7 +772,7 @@ public class DrawAction extends MapMode implements MapViewPaintable, DataSelecti
             if ((posn0 != -1 && // n0 is part of way
                 (posn0 >= 1                            && targetNode.equals(selectedWay.getNode(posn0-1)))) || // previous node
                 (posn0 < selectedWay.getNodesCount()-1 && targetNode.equals(selectedWay.getNode(posn0+1)))) {  // next node
-                getLayerManager().getEditDataSet().setSelected(targetNode);
+                setSelection(getLayerManager().getEditDataSet(), targetNode);
                 lastUsedNode = targetNode;
                 return true;
             }
@@ -788,7 +831,7 @@ public class DrawAction extends MapMode implements MapViewPaintable, DataSelecti
             tryToSetBaseSegmentForAngleSnap();
 
         computeHelperLine();
-        addHighlighting();
+        addHighlighting(e);
     }
 
     /**
@@ -960,7 +1003,7 @@ public class DrawAction extends MapMode implements MapViewPaintable, DataSelecti
             return;
         mousePos = e.getPoint();
         snapHelper.noSnapNow();
-        boolean repaintIssued = removeHighlighting();
+        boolean repaintIssued = removeHighlighting(e);
         // force repaint in case snapHelper needs one. If removeHighlighting
         // caused one already, don't do it again.
         if (!repaintIssued) {
@@ -1122,15 +1165,16 @@ public class DrawAction extends MapMode implements MapViewPaintable, DataSelecti
      * The status bar derives its information from oldHighlights, so in order to update the status
      * bar both addHighlighting() and repaintIfRequired() are needed, since former fills newHighlights
      * and latter processes them into oldHighlights.
+     * @param event event, can be null
      */
-    private void addHighlighting() {
+    private void addHighlighting(Object event) {
         newHighlights = new HashSet<>();
         MapView mapView = MainApplication.getMap().mapView;
 
         // if ctrl key is held ("no join"), don't highlight anything
         if (ctrl) {
             mapView.setNewCursor(cursor, this);
-            redrawIfRequired();
+            redrawIfRequired(event);
             return;
         }
 
@@ -1142,29 +1186,30 @@ public class DrawAction extends MapMode implements MapViewPaintable, DataSelecti
         if (mouseOnExistingNode != null) {
             mapView.setNewCursor(cursorJoinNode, this);
             newHighlights.add(mouseOnExistingNode);
-            redrawIfRequired();
+            redrawIfRequired(event);
             return;
         }
 
         // Insert the node into all the nearby way segments
         if (mouseOnExistingWays.isEmpty()) {
             mapView.setNewCursor(cursor, this);
-            redrawIfRequired();
+            redrawIfRequired(event);
             return;
         }
 
         mapView.setNewCursor(cursorJoinWay, this);
         newHighlights.addAll(mouseOnExistingWays);
-        redrawIfRequired();
+        redrawIfRequired(event);
     }
 
     /**
      * Removes target highlighting from primitives. Issues repaint if required.
+     * @param event event, can be null
      * @return true if a repaint has been issued.
      */
-    private boolean removeHighlighting() {
+    private boolean removeHighlighting(Object event) {
         newHighlights = new HashSet<>();
-        return redrawIfRequired();
+        return redrawIfRequired(event);
     }
 
     @Override
@@ -1313,16 +1358,7 @@ public class DrawAction extends MapMode implements MapViewPaintable, DataSelecti
     @Override
     public Collection<? extends OsmPrimitive> getPreservedPrimitives() {
         DataSet ds = getLayerManager().getEditDataSet();
-        if (ds != null) {
-            // Preserves selected primitives and selected way nodes
-            Set<OsmPrimitive> result = new HashSet<>();
-            for (Way w : ds.getSelectedWays()) {
-                result.addAll(w.getNodes());
-            }
-            result.addAll(ds.getSelected());
-            return result;
-        }
-        return Collections.emptySet();
+        return ds != null ? ds.allPreservedPrimitives() : Collections.emptySet();
     }
 
     @Override
@@ -1367,7 +1403,7 @@ public class DrawAction extends MapMode implements MapViewPaintable, DataSelecti
             }
             // select last added node - maybe we will continue drawing from it
             if (n != null) {
-                getLayerManager().getEditDataSet().addSelected(n);
+                addSelection(getLayerManager().getEditDataSet(), n);
             }
         }
     }

src/org/openstreetmap/josm/actions/mapmode/MapMode.java
@@ -232,8 +232,10 @@ public abstract class MapMode extends JosmAction implements MouseListener, Mouse
     /**
      * Gets a collection of primitives that should not be hidden by the filter.
      * @return The primitives that the filter should not hide.
+     * @deprecated use {@link org.openstreetmap.josm.data.osm.DataSet#allPreservedPrimitives}
      * @since 11993
      */
+    @Deprecated
     public Collection<? extends OsmPrimitive> getPreservedPrimitives() {
         return Collections.emptySet();
     }

src/org/openstreetmap/josm/data/osm/AbstractPrimitive.java
@@ -170,6 +170,11 @@ public abstract class AbstractPrimitive implements IPrimitive {
     protected static final short FLAG_ANNOTATED = 1 << 12;
 
     /**
+     * Determines if the primitive is preserved from the filter mechanism.
+     */
+    protected static final short FLAG_PRESERVED = 1 << 13;
+
+    /**
      * Put several boolean flags to one short int field to save memory.
      * Other bits of this field are used in subclasses.
      */

src/org/openstreetmap/josm/data/osm/DataSet.java
@@ -517,6 +517,16 @@ public final class DataSet extends QuadBucketPrimitiveStore implements Data, Pro
     }
 
     /**
+     * Returns a collection containing all primitives preserved from filtering.
+     * @return A collection containing all primitives preserved from filtering.
+     * @see OsmPrimitive#isPreserved
+     * @since 13309
+     */
+    public Collection<OsmPrimitive> allPreservedPrimitives() {
+        return getPrimitives(OsmPrimitive::isPreserved);
+    }
+
+    /**
      * Adds a primitive to the dataset.
      *
      * @param primitive the primitive.

src/org/openstreetmap/josm/data/osm/FilterMatcher.java
@@ -10,8 +10,6 @@ import org.openstreetmap.josm.data.osm.search.SearchCompiler.Match;
 import org.openstreetmap.josm.data.osm.search.SearchCompiler.Not;
 import org.openstreetmap.josm.data.osm.search.SearchMode;
 import org.openstreetmap.josm.data.osm.search.SearchParseError;
-import org.openstreetmap.josm.gui.MainApplication;
-import org.openstreetmap.josm.gui.MapFrame;
 import org.openstreetmap.josm.tools.SubclassFilteredCollection;
 
 /**
@@ -224,9 +222,7 @@ public class FilterMatcher {
     }
 
     private static FilterType test(List<FilterInfo> filters, OsmPrimitive primitive, boolean hidden) {
-        MapFrame map = MainApplication.getMap();
-        if (primitive.isIncomplete() ||
-                (map != null && map.mapMode != null && map.mapMode.getPreservedPrimitives().contains(primitive)))
+        if (primitive.isIncomplete() || primitive.isPreserved())
             return FilterType.NOT_FILTERED;
 
         boolean filtered = false;

src/org/openstreetmap/josm/data/osm/OsmPrimitive.java
@@ -427,6 +427,15 @@ public abstract class OsmPrimitive extends AbstractPrimitive implements Comparab
     }
 
     /**
+     * Set binary property used internally by the filter mechanism.
+     * @param isPreserved new "preserved" flag value
+     * @since 13309
+     */
+    public void setPreserved(boolean isPreserved) {
+        updateFlags(FLAG_PRESERVED, isPreserved);
+    }
+
+    /**
      * Replies true, if this primitive is disabled. (E.g. a filter applies)
      * @return {@code true} if this object has the "disabled" flag enabled
      */
@@ -456,6 +465,15 @@ public abstract class OsmPrimitive extends AbstractPrimitive implements Comparab
      */
     public boolean getDisabledType() {
         return (flags & FLAG_DISABLED_TYPE) != 0;
+    }
+
+    /**
+     * Replies true, if this primitive is preserved from filtering.
+     * @return {@code true} if this object has the "preserved" flag enabled
+     * @since 13309
+     */
+    public boolean isPreserved() {
+        return (flags & FLAG_PRESERVED) != 0;
     }
 
     /**